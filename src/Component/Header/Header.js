d06bef59dc4a95545e21f40842bfee9f52c7aeea
Murtaza K <m.kankoliwala@somaiya.edu>
2021-05-30 12:04:17 +0530

    new view

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,17 +1,18 @@
 import React, {useState} from 'react'
+import { Link } from 'react-router-dom'
 import './header.css'
 const Header = () => {
     return (
         <div>
             <ul className="nav justify-content-center">
                 <li className="nav-item text" >
-                    <a className="nav-link" href="/myprofile/" style={{color:'whitesmoke'}}>About</a>
+                    <Link className="nav-link" to="/myprofile/profile" style={{color:'whitesmoke'}}>About</Link>
                 </li>
                 <li className="nav-item text">
-                    <a className="nav-link" href="/myprofile/projects"  style={{color:'whitesmoke'}}>Projects</a>
+                    <Link className="nav-link" to="/myprofile/projects"  style={{color:'whitesmoke'}}>Projects</Link>
                 </li>
                 <li className="nav-item text">
-                    <a className="nav-link" href="/myprofile/work"  style={{color:'whitesmoke'}}>Work</a>
+                    <Link className="nav-link" to="/myprofile/work"  style={{color:'whitesmoke'}}>Work</Link>
                 </li>
                 </ul>
         </div>
